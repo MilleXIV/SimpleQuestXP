3a169111959c346946e937650452b29c70b99140
MilleXIV <millexiv@gmail.com>
2020-08-31 13:07:57 -0400

    Fix up bugs with all reward types

--- SimpleQuestXP.lua
+++ SimpleQuestXP.lua
@@ -1,11 +1,40 @@
 local GetQuestLogRewardXP = GetQuestLogRewardXP
-local choosePoint, chooseParent, chooseRelPoint, chooseXOff, chooseYOff = QuestLogItemChooseText:GetPoint()
 local SimpleQuestXPText = QuestLogDetailScrollChildFrame:CreateFontString("SimpleQuestXPText", "BACKGROUND", "QuestFont")
 
-SimpleQuestXPText:SetPoint(choosePoint, chooseParent, chooseRelPoint, chooseXOff, chooseYOff)
-QuestLogItemChooseText:SetPoint(choosePoint, SimpleQuestXPText, chooseRelPoint, chooseXOff, chooseYOff)
+local choosePoint, _, chooseRelPoint, chooseXOff, chooseYOff = QuestLogItemChooseText:GetPoint()
+SimpleQuestXPText:SetPoint(choosePoint, QuestLogRewardTitleText, chooseRelPoint, chooseXOff, chooseYOff)
+SimpleQuestXPText:Hide()
 
 local function selectQuestHook(qIndex)
-    SimpleQuestXPText:SetText(EXPERIENCE_COLON.." "..GetQuestLogRewardXP())
+    local xp = GetQuestLogRewardXP()
+    local numRewards = GetNumQuestLogRewards()
+	local numChoices = GetNumQuestLogChoices()
+    local money = GetQuestLogRewardMoney()
+    local numSpells = GetNumQuestLogRewardSpells()
+
+    if not QuestLogRewardTitleText:IsShown() and (xp + numRewards + numChoices + numSpells + money) > 0 then
+        QuestLogRewardTitleText:Show()
+    end
+
+    if xp > 0 then
+        SimpleQuestXPText:SetText(EXPERIENCE_COLON.." "..xp)
+        SimpleQuestXPText:Show()
+        
+        if numChoices > 0 then
+            local choosePoint, _, chooseRelPoint, chooseXOff, chooseYOff = QuestLogItemChooseText:GetPoint()
+            QuestLogItemChooseText:SetPoint(choosePoint, SimpleQuestXPText, chooseRelPoint, chooseXOff, chooseYOff)
+            return qIndex
+        end
+        if numSpells > 0 then
+            local spellPoint, _, spellRelPoint, spellXOff, spellYOff = QuestLogSpellLearnText:GetPoint()
+            QuestLogSpellLearnText:SetPoint(spellPoint, SimpleQuestXPText, spellRelPoint, spellXOff, spellYOff)
+            return qIndex
+        end
+        if (numRewards + money) > 0 then
+            local recPoint, _, recRelPoint, recXOff, recYOff = QuestLogItemReceiveText:GetPoint()
+            QuestLogItemReceiveText:SetPoint(recPoint, SimpleQuestXPText, recRelPoint, recXOff, recYOff)
+            return qIndex
+        end
+    end
 end
-hooksecurefunc("SelectQuestLogEntry", selectQuestHook)
+hooksecurefunc("QuestLog_UpdateQuestDetails", selectQuestHook)
